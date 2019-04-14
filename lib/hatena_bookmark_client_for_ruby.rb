18cf88e9ab478b166e3d035b6ada03cce1a1fb78
t__nabe <tanabe.1478@gmail.com>
2019-04-14 22:32:45 +0900

    Add Entry class, BookmarkCount class, and search method

--- lib/hatena_bookmark_client_for_ruby.rb
+++ lib/hatena_bookmark_client_for_ruby.rb
@@ -1,8 +1,53 @@
 require "hatena_bookmark_client_for_ruby/version"
 require "oauth"
 require "crack"
+require "net/http"
 
 module HatenaBookmarkClientForRuby
+  class BookmarkCount
+    attr_accessor :urls
+
+    def initialize(urls)
+      @urls = urls
+    end
+
+    def get
+      uri = URI.parse("http://api.b.st-hatena.com/entry.counts#{query_parameter}")
+      response = Net::HTTP.get_response(uri)
+    end
+
+    private
+
+    def query_parameter
+      encoded_urls = urls.map { |url| CGI.escape(url) }
+      "?url=#{encoded_urls.join("&url=")}"
+    end
+  end
+
+  class Entry
+    attr_accessor :url
+
+    def initialize(url)
+      @url = url
+    end
+
+    def get()
+      encoded = URI.encode(url)
+      uri = URI.parse("http://b.hatena.ne.jp/entry/json/#{url}")
+      http = Net::HTTP.new(uri.host, uri.port)
+      headers = { "content-Type" => "application/json" }
+      http.get(uri.path, headers)
+    end
+
+    def get_withou_relations()
+      encoded = URI.encode(url)
+      uri = URI.parse("http://b.hatena.ne.jp/entry/jsonlite/#{url}")
+      http = Net::HTTP.new(uri.host, uri.port)
+      headers = { "content-Type" => "application/json" }
+      http.get(uri.path, headers)
+    end
+  end
+
   class Bookmark
     attr_accessor :access_token
 
@@ -44,6 +89,10 @@ module HatenaBookmarkClientForRuby
 
     def user()
       access_token.get("http://api.b.hatena.ne.jp/1/my")
+    end
+
+    def search(id, query, of = 0, limit = 20)
+      access_token.get("http://b.hatena.ne.jp/#{id}/search/json?q=#{query.encode("UTF-8")}&of=#{of}&limit=#{limit}")
     end
   end
 
